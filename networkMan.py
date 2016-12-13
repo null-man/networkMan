44e131ac7202c4fd641970181cefc96f16d25422
edison <635384073@qq.com>
2016-12-13 16:03:19 +0800

    V1.2 修复部分bug以及重构部分代码

--- networkMan.py
+++ networkMan.py
@@ -5,27 +5,38 @@ import os
 import time
 
 class NetworkMan:
-    def __init__(self, phantomjs, netlogjs, url, path, split):
+    def __init__(self, phantomjs, netlogjs, url, path, split = None, filter_word = None):
         # phantomjs 路径
         self.phantomjs = phantomjs
+        
         # netlog 路径
         self.netlogjs = netlogjs
+        
         # 请求的url
         self.url = url
+
+        # 默认过滤url为当前url域名
+        if split == None:
+            split = (url.split(".com/")[0] + ".com").split("http://")[1]
+
         # 切割点(一般以域名为切割点, 用户还原目录结构)
-        self.split = split
+        self.split = split + "/"
+
+        # 默认network 拦截域名为当前url域名
+        if filter_word == None:
+            filter_word = split
 
         # 本地存放路径
-        self.root_path = path + str(int(time.time())) + "/"
+        self.root_path = path + "/" +  str(int(time.time())) + "/"
         self.path = self.root_path + "index.txt"
 
         # 创建索引文件
         self.createFile(self.path)
 
         # 获取 network所有记录的命令
-        self.cmd = " ".join([self.phantomjs, self.netlogjs, self.url, self.path])
+        self.cmd = " ".join([self.phantomjs, self.netlogjs, self.url, self.path, filter_word])
 
-
+        print "cmd: " + self.cmd
 
     # 创建多级目录以及文件
     def createFile(self, path, content = None):
@@ -44,8 +55,6 @@ class NetworkMan:
             f.close()
             print "文件创建完成: "+ path
 
-
-
     # 所有network记录写入index文件
     def getNetLog(self):
         # 执行phantomjs 命令
@@ -53,16 +62,15 @@ class NetworkMan:
         p = os.popen(self.cmd)
         print p.read()
 
-
-
     # 读取所有network记录 爬取文件
     def savePageInfo(self, url):
             # 去除空格
             file = requests.get(url.strip())
-            # 文件名
-            file_name = ((url.split('4399.com/')[1])[::-1].split("/")[0])[::-1].strip()
-            # 去除文件名的路径
-            path = (url.split('4399.com/')[1]).split(file_name)[0]
+            # 本地文件名
+            file_name = ((url.split(self.split)[1])[::-1].split("/")[0])[::-1].strip()
+            # 本地去除文件名的路径
+            path = (url.split(self.split)[1]).split(file_name)[0]
+            # 本地路径
             path = self.root_path + path
 
             print "写入目录:" + path
@@ -79,8 +87,7 @@ class NetworkMan:
             print "创建文件成功:" + path
 
 
-
-    # 主函数
+    # 开始工作
     def do(self):
         # 获取network记录
         self.getNetLog()
@@ -102,11 +109,15 @@ class NetworkMan:
 
 
 phantomjs = "/Users/edison/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs"
-netlogjs  = "/Users/edison/Downloads/phantomjs-2.1.1-macosx/examples/netlog.js"
-url       = "http://sda.4399.com/4399swf/upload_swf/ftp19/ssj/20160926/b3/index.htm"
-path      = "/Users/edison/Desktop/temp/"
-split_word= "4399.com"
+netlogjs  = "/Users/edison/Downloads/phantomjs-2.1.1-macosx/examples/netlog0.js"
+url       = "http://www.17sucai.com/preview/161350/2016-05-17/products-WB098M36S/index.html"
+path      = "/Users/edison/Desktop/temp"
+
 
 # 初始化爬虫
-networkMan = NetworkMan(phantomjs, netlogjs, url, path, split_word)
+networkMan = NetworkMan(phantomjs, netlogjs, url, path)
 networkMan.do()
+
+
+
+
--- phantomjs/netlog.js
+++ phantomjs/netlog.js
@@ -1,75 +1,57 @@
 "use strict";
 var page    = require('webpage').create(),
     system  = require('system'),
-    index_path,
+    resources,
+    path,
+    filter,
     address;
-
-
-// 遍历network记录到文件
-var saveNetwork = function(urls, path, callbackFinal) {
-    var retrieve;
-
-    retrieve = function() {
-        var url;
-        if (urls.length > 0) {
-            url = urls.shift();
-
-            // 保存文件
-            saveFile(path, url)
-            
-            // 递归
-            retrieve()
-        } else {
-            return callbackFinal();
-        }
-    };
-
-    return retrieve();
-};
-
-// 写入文件
-var saveFile = function(path, url) {
-    try {
-        var fs = require('fs');
-        fs.write(path, url + "\r\n", 'w+');
-    } catch(e) {
-        console.log(e)
-    }
-    
-}
-
 
 
 if (system.args.length === 1) {
     console.log('Usage: netlog.js <some URL>');
     phantom.exit(1);
 } else {
-    address         = system.args[1];
-    index_path      = system.args[2];
-    page.resources  = []
+    address     = system.args[1];
+    resources   = [];
+    path        = system.args[2];
+    filter      = system.args[3];
 
     page.onResourceRequested = function (req) {
-        _url = JSON.parse(JSON.stringify(req, undefined, 4)).url
+        try {
+            var _url = JSON.parse(JSON.stringify(req, undefined, 4)).url
+            console.log("hook_url: " + _url);
 
-        // 只获取该域名的数据
-        if (_url.indexOf('4399.com') > 0) {
-            page.resources.push(_url)
-            console.log("网络请求: " + _url);
-        }
+            if (_url.indexOf(filter) > 0) {
+                resources.push(_url)
+            }
+
+        }catch(e) {
+            console.log(e)
+        }  
     };
 
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('FAIL to load the address');
-        } else {
-            // 保存index.html文件
-            saveFile(index_path, address)
-
-            // 遍历保存network
-            saveNetwork(page.resources, index_path, function(){
-                console.log("========== [网络请求收集完成] ==========")
-                return phantom.exit();
-            });   
         }
+        window.setTimeout(function () {
+            try {
+                var fs = require('fs');
+                
+                console.log("建立索引文件 ...")
+
+                while (resources.length > 0) {
+                    var url = resources.shift();
+                    fs.write(path, url + "\r\n", 'w+');
+
+                    console.log("写入: " + url)
+                }
+            } catch(e) {
+                console.log(e)
+            }
+
+            console.log("索引文件写入完毕 ...")
+            phantom.exit(1);
+        }, 10000);
     });
 }
